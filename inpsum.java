eec263e423e456fe09f959e951aaeeec59796337
srajan333 <158080328+srajan333@users.noreply.github.com>
2024-09-10 00:25:58 +0530

    sum of input

---
+++ inpsum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class inpsum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("a=");
+        int a = sc.nextInt();
+        System.out.print("b=");
+        int b = sc.nextInt();
+        System.out.print("Sum of a & b is:");
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
--- input.java
+++ input.java
@@ -1,4 +1,6 @@
-
+/**
+ * input
+ */
 import java.util.*;
 public class input {
 
