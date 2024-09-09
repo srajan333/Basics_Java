662a7065939f718882607ffefed217f4b38475a6
srajan333 <158080328+srajan333@users.noreply.github.com>
2024-09-10 00:46:05 +0530

    important code

---
+++ areacircle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class areacircle {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Radius:");
+       float r = sc.nextFloat();
+       System.out.print("Area of circle is:");
+       float area = 3.14f*r*r;
+       System.out.println(area);
+
+
+    }
+}
---
+++ inputproduct.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class inputproduct {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("a=");
+        int a = sc.nextInt();
+        System.out.print("b=");
+        int b = sc.nextInt();
+        System.out.print("Product of a&b will be:");
+        int product = a*b;
+        System.out.println(product);
+    }
+}
