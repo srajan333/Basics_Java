2a5c46bc11ce3a04ca9ef23800c2ff956f3eb5dc
srajan333 <158080328+srajan333@users.noreply.github.com>
2024-09-13 23:09:34 +0530

    typecasting

---
+++ typecasting.java
@@ -0,0 +1,11 @@
+public class typecasting {
+    public static void main(String[] args) {
+        //Without typecasting:
+        //float a = 10.0;
+        //int b = a;
+        //with type casting;
+        float a = 10.0f;
+        int b = (int)a;
+        System.out.println(b);
+    }
+}
