45d8184dfeefc6df4374f58e41bc7c7e31abaa4f
srajan333 <158080328+srajan333@users.noreply.github.com>
2024-10-01 15:43:24 +0530

    sum modified

--- inpsum.java
+++ inpsum.java
@@ -5,9 +5,11 @@ import java.util.*;
 public class inpsum {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("First number=");
+    System.out.print("First number is:");
     int a = sc.nextInt();
+    System.out.print("Second number is:");
     int b = sc.nextInt();
+    System.out.println("Sum is:");
     int sum = a+b;
     System.out.println(sum);
 }
