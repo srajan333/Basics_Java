537a8a93eb3f166d1b254fd0b10079f256e4924a
srajan333 <158080328+srajan333@users.noreply.github.com>
2024-11-01 19:19:21 +0530

    operators

--- ar.java
+++ ar.java
@@ -1,14 +1,7 @@
 public class ar {
     public static void main(String[] args) {
-      int time = 24;
-      if (time < 12 ) {
-        System.out.println("Good Morning");
-      }
-      else if (time < 18) {
-        System.out.println("Good Day");
-      }
-      else {
-        System.out.println("Good Evening");
-      }
+      int x = 20;
+      
+     System.out.println(!(x<23 || x>19));
     }
 }
